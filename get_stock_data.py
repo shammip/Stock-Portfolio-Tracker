eb2c1a2ec8da8f80d5bb8d8a8cebb1ba9db17cb7
James Giannoni <jgiannoni1@gmail.com>
2025-04-14 21:52:23 +0000

    add additional error handling to get_stock_data. created separate unit testing file as well

--- get_stock_data.py
+++ get_stock_data.py
@@ -11,35 +11,49 @@ put your email in the placeholder inside the Main function.
 
 # will be used to retrieve more details on a given stock
 def get_stock_details(ticker):
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period="2d")
-    info = stock.info
+    try:
+        stock = yf.Ticker(ticker)
+        hist = stock.history(period="2d")
+        info = stock.info
+        
+        # handle cases where a brand new stock is searched for and there isn't enough data
+        if hist.empty:
+            raise ValueError("Not enough data to calculate.")
+        
+        if len(hist) < 2:
+            raise ValueError("Not enough data to calculate.")
+
+        # helper function to get annual revenue
+        def get_annual_revenue(stock):
+            try:
+                income_stmt = stock.income_stmt
+                latest_date = income_stmt.columns[0]
+                latest_revenue = income_stmt.loc["Total Revenue", latest_date]
+                return latest_revenue # this will always be for the year prior to current year. It is now 2025 so it will be for 2024
+            except Exception:
+                return None
+            
+        stock_data = {
+            "ticker_symbol": ticker,
+            "company_name": info["displayName"] if "displayName" in info else None,
+            "industry": info["industry"] if "industry" in info else None,
+            "cur_price": info["currentPrice"] if "currentPrice" in info else None,
+            "pct_change": ((hist['Close'].iloc[-1] - hist['Close'].iloc[-2]) / hist['Close'].iloc[-2]) * 100,
+            "day_open": hist['Open'].iloc[-1] if 'Open' in hist.columns else None,
+            "day_low": hist['Low'].iloc[-1] if 'Low' in hist.columns else None,
+            "day_high": hist['High'].iloc[-1] if 'High' in hist.columns else None,
+            "52wk_low": info["fiftyTwoWeekLow"] if "fiftyTwoWeekLow" in info else None,
+            "52wk_high": info["fiftyTwoWeekHigh"] if "fiftyTwoWeekHigh" in info else None,
+            "market_cap": info["marketCap"] if "marketCap" in info else None,
+            "curr_volume": info["volume"] if "volume" in info else None,
+            "avg_volume": info["averageVolume"] if "averageVolume" in info else None,
+            "annual_revenue": get_annual_revenue(stock)
+        }
+
+        return stock_data # dictionary
     
-    # helper function to get annual revenue
-    def get_annual_revenue(stock):
-        income_stmt = stock.income_stmt
-        latest_date = income_stmt.columns[0]
-        latest_revenue = income_stmt.loc["Total Revenue", latest_date]
-        return latest_revenue # this will always be for the year prior to current year. It is now 2025 so it will be for 2024
-
-    stock_data = {
-        "ticker_symbol": ticker,
-        "company_name": info["displayName"] if "displayName" in info else None,
-        "industry": info["industry"] if "industry" in info else None,
-        "cur_price": info["currentPrice"] if "currentPrice" in info else None,
-        "pct_change": ((hist['Close'].iloc[-1] - hist['Close'].iloc[-2]) / hist['Close'].iloc[-2]) * 100,
-        "day_open": hist['Open'].iloc[-1] if 'Open' in hist.columns else None,
-        "day_low": hist['Low'].iloc[-1] if 'Low' in hist.columns else None,
-        "day_high": hist['High'].iloc[-1] if 'High' in hist.columns else None,
-        "52wk_low": info["fiftyTwoWeekLow"] if "fiftyTwoWeekLow" in info else None,
-        "52wk_high": info["fiftyTwoWeekHigh"] if "fiftyTwoWeekHigh" in info else None,
-        "market_cap": info["marketCap"] if "marketCap" in info else None,
-        "curr_volume": info["volume"] if "volume" in info else None,
-        "avg_volume": info["averageVolume"] if "averageVolume" in info else None,
-        "annual_revenue": get_annual_revenue(stock)
-    }
-
-    return stock_data # dictionary
+    except Exception as e:
+        return {f"Could not fetch details for {ticker}: {str(e)}"}
 
 
 # used only for the watchlist where a subset of information, namely ticker, price, and percent of change are depicted.
@@ -47,55 +61,71 @@ def get_watchlist_data(tickers):
     stocks_data = {}
 
     for ticker in tickers:
-        stock = yf.Ticker(ticker)
-        hist = stock.history(period="2d")
-        info = stock.info
+        try:
+            stock = yf.Ticker(ticker)
+            hist = stock.history(period="2d")
+            info = stock.info
+            
+            if hist.empty:
+                raise ValueError("Not enough data to calculate.")
         
-        stocks_data[ticker] = {
-            "ticker_symbol": ticker,
-            "cur_price": info["currentPrice"] if "currentPrice" in info else None,
-            # maybe use pct_change to indicate red or green so that we can but the actual ppercentage where stock details is
-            "pct_change": ((hist['Close'].iloc[-1] - hist['Close'].iloc[-2]) / hist['Close'].iloc[-2]) * 100
-        }
+            if len(hist) < 2:
+                raise ValueError("Not enough data to calculate.")
+
+            stocks_data[ticker] = {
+                "ticker_symbol": ticker,
+                "cur_price": info["currentPrice"] if "currentPrice" in info else None,
+                # maybe use pct_change to indicate red or green so that we can but the actual ppercentage where stock details is
+                "pct_change": ((hist['Close'].iloc[-1] - hist['Close'].iloc[-2]) / hist['Close'].iloc[-2]) * 100
+            }
+        except Exception as e:
+            stocks_data[ticker] = {f"Could not fetch details for {ticker}: {str(e)}"}
 
     return stocks_data # dictionary of dictionaries (stonks(stonk: stuff about stonk))
 
 
 # uses yfinance news method which fetches articles from different publications
 def get_stock_news(ticker):
-    stock = yf.Ticker(ticker)
-    news = stock.news[:10]  # get only 10
+    try:
+        stock = yf.Ticker(ticker)
+        news = stock.news[:10]  # get only 10
 
-    filtered_news = []
-    for article in news:
-        content = article.get('content', {})
-        filtered_article = {
-            'title': content.get('title'),
-            'url': content.get('canonicalUrl', {}).get('url'),
-            'publisher': content.get('provider', {}).get('displayName'),
-            'pubDate': content.get('pubDate')
-        }
-        filtered_news.append(filtered_article)
+        filtered_news = []
+        for article in news:
+            content = article.get('content', {})
+            filtered_article = {
+                'title': content.get('title'),
+                'url': content.get('canonicalUrl', {}).get('url'),
+                'publisher': content.get('provider', {}).get('displayName'),
+                'pubDate': content.get('pubDate')
+            }
+            filtered_news.append(filtered_article)
 
-    return filtered_news # list of dictionaries
+        return filtered_news # list of dictionaries
+    
+    except Exception as e:
+        return [{f"Could not fetch news for {ticker}: {str(e)}"}]
 
 
 # uses edgartools to extract SEC filing documents
 def get_stock_filings(userEmail, ticker):
-    set_identity(userEmail)
-    ticker_symbol = Company(ticker)
-    filings = ticker_symbol.get_filings()
-    df = filings.to_pandas()
-    df = df[['filing_date', 'primaryDocDescription', 'primaryDocument', 'accession_number']].head(10)
-    
-    # create full urls 
-    df['document_url'] = df.apply(
-        lambda row: f"https://www.sec.gov/Archives/edgar/data/{str(ticker_symbol.cik).replace('-', '')}/{str(row['accession_number']).replace('-', '')}/{row['primaryDocument']}",
-        axis=1
-    )
+    try:
+        set_identity(userEmail)
+        ticker_symbol = Company(ticker)
+        filings = ticker_symbol.get_filings()
+        df = filings.to_pandas()
+        df = df[['filing_date', 'primaryDocDescription', 'primaryDocument', 'accession_number']].head(10)
 
-    # remove accession_number
-    return df[['filing_date', 'primaryDocDescription', 'document_url']] # dataframe with columns 'filing_date', 'primaryDocDescription', 'document_url'
+        # create full urls 
+        df['document_url'] = df.apply(
+            lambda row: f"https://www.sec.gov/Archives/edgar/data/{str(ticker_symbol.cik).replace('-', '')}/{str(row['accession_number']).replace('-', '')}/{row['primaryDocument']}",
+            axis=1
+        )
+
+        # remove accession_number
+        return df[['filing_date', 'primaryDocDescription', 'document_url']] # dataframe with columns 'filing_date', 'primaryDocDescription', 'document_url'
+    except Exception as e:
+        return pd.DataFrame([{f"Could not fetch filings for {ticker}: {str(e)}"}])
 
     
 # main function used for testing / debugging only
@@ -108,7 +138,7 @@ def main():
         ticker = input().strip().upper()
         stock_data = get_stock_details(ticker)
         stock_news = get_stock_news(ticker)
-        stock_filings = get_stock_filings('PUT YOUR EMAIL HERE', ticker)
+        stock_filings = get_stock_filings('stocktracker@gmail.com', ticker) #fake email works just fine
         print(stock_data) 
         print()
         print(stock_filings)
